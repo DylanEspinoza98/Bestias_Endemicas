909f266b422aec741b5bb61786b0d05774743fd6
nramirezAndroid <n.ramirez48@alumnos.santotomas.cl>
2025-09-08 00:59:45 -0300

    Nueva zona y más animales
    se agrego una nueva zona(austral) y se agregaron mas animales

--- app/src/main/java/com/example/bestiasendemicas/MainActivity.java
+++ app/src/main/java/com/example/bestiasendemicas/MainActivity.java
@@ -46,6 +46,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ImageButton cambiarPantalla4 = findViewById(R.id.btnAustral);
+        cambiarPantalla4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity_Austral.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
